63a85199fa879e55415af9eead66705c787e17ba
ArmandoMarioto <armando_marioto100@hotmail.com>
2022-07-27 12:50:04 -0300

    Contexto de autenticação

---
+++ pages/index.tsx
@@ -0,0 +1,26 @@
+import { FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import styles from '../styles/Home.module.css'; 
+
+export default function Home() {
+  const [email , setEmail] = useState('');
+  const [password , setPassword] = useState('');
+
+  const { signIn} = useContext(AuthContext);
+
+  async function handleSubmit(event: FormEvent){
+    event.preventDefault();
+    const data = {
+      email,
+      password
+    }
+    await signIn(data);
+  }
+  return (
+    <form className={styles.container} onSubmit={handleSubmit}>
+      <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email"/>
+      <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Senha"/>
+      <button type="submit">Enter</button>
+    </form>
+  );
+}
