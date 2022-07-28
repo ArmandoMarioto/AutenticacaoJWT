26c2fe31e268f11deab136d8d489055ef26039be
ArmandoMarioto <armando_marioto100@hotmail.com>
2022-07-28 13:19:21 -0300

    Realizando refresh token

--- service/api.ts
+++ service/api.ts
@@ -1,12 +1,41 @@
-import axios from "../node_modules/axios/index";
-import { parseCookies } from "nookies";
-
-const cookies = parseCookies();
+import axios,{AxiosError} from "../node_modules/axios/index";
+import { parseCookies, setCookie } from "nookies";
+interface AxiosErrorResponse {
+    code?: string;
+  }
+let cookies = parseCookies();
 
 export const api =  axios.create({
     baseURL: 'http://localhost:3333',
     headers: {
         Authorization: `Bearer ${cookies['nextauth.token']}`,
 
+    }
+});
+
+api.interceptors.response.use(response => {
+    return response;
+}, (error: AxiosError<AxiosErrorResponse>) => {
+    if(error.response.status === 401) {
+        if(error.response.data?.code === 'token.expired'){
+            cookies = parseCookies();
+            const {'nextauth.refreshToken': refreshToken} = cookies;
+            api.post('refresh', {refreshToken}).then(response => {
+                const { token } = response.data;
+
+                setCookie(undefined, 'nextauth.token', token, {
+                    maxAge: 30 * 24 * 60 * 60,
+                    path: '/',
+                  })
+                  setCookie(undefined, 'nextauth.refreshToken', response.data.refreshToken, {
+                    maxAge: 30 * 24 * 60 * 60,
+                    path: '/',
+                  })
+
+                    api.defaults.headers['Authorization'] = `Bearer ${token}`;
+            })
+        }else {
+
+        }
     }
 })
